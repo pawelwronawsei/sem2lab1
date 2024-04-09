c4d5de96922f363cf10d2904e601cd2bfc83acb9
pawel.wrona <pawel.wrona@microsoft.wsei.edu.pl>
2024-04-09 11:26:03 +0200

    sem2lab6

---
+++ sem2lab6/Customer.cs
@@ -0,0 +1,7 @@
+﻿namespace sem2lab6;
+
+public partial class Customer
+{
+    public string Name { get; set; }
+    public int Id { get; set; }
+}
---
+++ sem2lab6/MainWindow.cs
@@ -0,0 +1,16 @@
+﻿namespace sem2lab6;
+
+public partial class MainWindow
+{
+    public void GenerateButtons(StackPanel panel)
+    {
+        var team = new Team()
+        {
+            Lead = "Adam", 
+            Architect = "Ewa", 
+            Testers = new[] { "Karol", "Adrian" },
+            Developers = new[] { "Jan", "Grzegorz" }
+        };
+        
+    }
+}
