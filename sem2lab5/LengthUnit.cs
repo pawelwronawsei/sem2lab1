4dbeb74c28e3c757602fd09db9aeed7984bbe4a3
pawel.wrona <pawel.wrona@microsoft.wsei.edu.pl>
2024-04-09 11:26:22 +0200

    c2

---
+++ sem2lab5/LengthUnit.cs
@@ -0,0 +1,6 @@
+﻿namespace sem2lab5;
+
+public enum LengthUnit
+{
+    MM = 1, CM = 10, M = 1_000, KM = 1_000_000, FT = 304
+}
---
+++ sem2lab5/LengthUnitExtension.cs
@@ -0,0 +1,22 @@
+﻿namespace sem2lab5;
+
+public static class LengthUnitExtension
+{
+    public static double ToDouble(this LengthUnit unit)
+    {
+        return unit switch
+        {
+            LengthUnit.FT => 304.80,
+            _ => (int)unit
+        };
+    }
+    
+    public static decimal ToDecimal(this LengthUnit unit)
+    {
+        return unit switch
+        {
+            LengthUnit.FT => (decimal)304.80,
+            _ => (decimal)unit
+        };
+    }
+}
