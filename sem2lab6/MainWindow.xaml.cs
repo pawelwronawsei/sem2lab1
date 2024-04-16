82ee5acc1f2f3ddca9e4cca7269da0427740ba6f
pawel.wrona <pawel.wrona@microsoft.wsei.edu.pl>
2024-04-16 11:28:07 +0200

    commit1604

---
+++ sem2lab6/MainWindow.xaml.cs
@@ -0,0 +1,187 @@
+﻿using System.Collections;
+using System.Text;
+using System.Transactions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace sem2lab6;
+
+public partial class Customer
+{
+    public string Address { get; set; }
+}
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        Customer customer = new Customer()
+        {
+            Address = "Św. Filipa 17";
+            Name = "";
+        };
+
+        for (var i = 1; i <= team.Length; i++)
+        {
+            Button button = new Button()
+            {
+                Content = team[i]
+            };
+            Panel.Children.Add(button);
+        }
+        
+        // for (var enumerator = team.GetEnumerator(); enumerator.MoveNext();)
+        // {
+        //     var member = enumerator.Current;
+        // }
+        //
+        // foreach (var member in team)
+        // {
+        //     Button button = new Button()
+        //     {
+        //         Content = member
+        //     };
+        //     Panel.Children.Add(button);
+        // }
+
+        Info.Content = team[1];
+    }
+}
+
+class Team : IEnumerable<string>
+{
+    public string Lead { get; set; }
+    public string? Architect { get; set; }
+    public string[] Testers { get; set; }
+    public string[] Developers { get; set; }
+
+    public int Length
+    {
+        get
+        {
+            return (1 + (Architect is null ? 0 : 1) + Testers.Length + Developers.Length);
+        }
+    }
+
+    public string this[int i]
+    {
+        get
+        {
+            switch(i)
+            {
+               case 1: return Lead;
+               case 2: return Architect;
+               default:
+                   if (i > 2 && i <= Testers.Length + 2)
+                   {
+                       return Testers[i - 2];
+                   }else if (i > 2 + Testers.Length && i <= Developers.Length + Testers.Length + 2)
+                   {
+                       return Developers[i - 3 - Testers.Length];
+                   }
+                   throw new IndexOutOfRangeException();
+            }
+        }
+        set
+        {
+            
+        }
+    }
+
+    public class TeamEnumerator : IEnumerator<string>
+    {
+        private string[] _items;
+
+        public TeamEnumerator(Team team)
+        {
+            int size = 1 + team.Testers.Length + team.Developers.Length + (team.Architect == null ? 1 : 0);
+            _items = new string[size];
+            _items[0] = team.Lead;
+            if (team.Architect == null)
+            {
+                for (int i = 0; i < team.Testers.Length; i++)
+                {
+                    _items[i + 1] = team.Testers[i];
+                }
+                for (int i = 0; i < team.Testers.Length; i++)
+                {
+                    _items[i + 1 + team.Testers.Length] = team.Developers[i];
+                }
+            }
+            else
+            {
+                _items[1] = team.Architect;
+                for (int i = 0; i < team.Testers.Length; i++)
+                {
+                    _items[i + 2] = team.Testers[i];
+                }
+                for (int i = 0; i < team.Developers.Length; i++)
+                {
+                    _items[i + 2 + team.Testers.Length] = team.Developers[i];
+                }
+            }
+        }
+
+        private int _index = -1;
+        
+        public bool MoveNext()
+        {
+            return ++_index < _items.Length;
+        }
+
+        public void Reset()
+        {
+            _index = -1;
+        }
+
+        public string Current
+        {
+            get
+            {
+                return _items[_index];
+            }
+        }
+
+        object IEnumerator.Current => Current;
+
+        public void Dispose()
+        {
+        }
+    } 
+    public IEnumerator<string> GetEnumerator()
+    {
+        // return new TeamEnumerator(this);
+        yield return Lead;
+        if (Architect != null)
+        {
+            yield return Architect;
+        }
+
+        foreach (var tester in Testers)
+        {
+            yield return tester;
+        }
+
+        foreach (var dev in Developers)
+        {
+            yield return dev;
+        }
+    }
+    
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
---
+++ sem2lab7/Program.cs
@@ -0,0 +1,138 @@
+﻿using System.Reflection;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        IList<string> names = new List<string>() { "Ewa", "Adam", "Karol" };
+        names.Add("Robert");
+        names.Remove("Ewa");
+        names.RemoveAt(0);
+        names[0] = "Ewa";
+        Console.WriteLine(string.Join(", ", names));
+        
+        // ZAMIANA OSTATNIEGO Z PIERWSZYM
+        (names[0], names[names.Count - 1]) = (names[names.Count - 1], names[0]);
+        Console.WriteLine(string.Join(", ", names));
+        
+        names.Add("Beata");
+        Console.WriteLine(string.Join(", ", names));
+        
+        // OSTATNI IDZIE NA POCZĄTEK
+        names.Insert(0, names.Last());
+        names.RemoveAt(names.Count - 1);
+        Console.WriteLine(string.Join(", ", names));
+
+        // LINKEDLIST
+        string[] arr = { "b", "a", "c" };
+        LinkedList<string> linked = new LinkedList<string>(arr);
+        var first = linked.First;
+        Console.WriteLine(first.Value);
+        
+        // DODANIE NOWEGO ELEMENTU ZA PIERWSZYM
+        linked.AddAfter(first, new LinkedListNode<string>("z"));
+        Console.WriteLine(string.Join(", ", linked));
+
+        // ZADANIE Z PLAYERS
+        
+        var players = new List<Player>()
+        {
+            new Player(){ Id = 1, Name = "Adam", Points = 123 },
+            new Player(){ Id = 2, Name = "Karol", Points = 30 },
+            new Player(){ Id = 3, Name = "Ania", Points = 330 },
+            new Player(){ Id = 3, Name = "Ania", Points = 330 },
+        };
+        
+        // TRZEBA DODAĆ EQUALS ABY TO DZIAŁAŁO (LUPA -> GEN CODE -> EQUALITY MEMBERS)
+        Console.WriteLine(players.IndexOf(new Player(){ Id = 1 }));
+        
+        // wyświetl graczy z imieniem zaczynającym się na literę a
+        players.ForEach(player =>
+        {
+            if(player.Name.ToLower()[0] == 'a') Console.WriteLine(player.Name);
+        });
+        
+        // UŻYCIE SET
+        ISet<Player> room = new HashSet<Player>(players);
+        room.Add(new Player() { Id = 4, Name = "Paweł", Points = 100 });
+        Console.WriteLine(string.Join(", ", room));
+        Console.WriteLine(room.Contains(new Player() { Id = 5 }));
+        ISet<Player> team = new HashSet<Player>();
+        team.Add(players[1]);
+        team.Add(new Player() { Id = 6, Name = "Stefan", Points = 12 });
+        
+        // CZĘŚĆ WSPÓLNA ROOM I TEAM
+        var intersect = new HashSet<Player>(room); // kopiowanie zbioru
+        intersect.IntersectWith(team);
+        Console.WriteLine(string.Join(", ", intersect));
+        
+        // ALFABETYCZNIE
+        ISet<string> sortedNames = new SortedSet<string>(names);
+        sortedNames.Add("Alicja");
+        Console.WriteLine(string.Join(", ", sortedNames));
+        ISet<Player> sortedPlayers = new SortedSet<Player>(players);
+        Console.WriteLine(string.Join(", ", sortedPlayers));
+        IDictionary<string, Player> playersDictionary = new Dictionary<string, Player>();
+        playersDictionary.Add("adam@wsei.edu.pl", players[0]);
+        playersDictionary.Add("karol@wsei.edu.pl", players[1]);
+        playersDictionary.Add("ania@wsei.edu.pl", players[2]);
+        Console.WriteLine(string.Join(", ", playersDictionary));
+        Console.WriteLine(playersDictionary["ania@wsei.edu.pl"]);
+        
+        // dodaj 10 punktów graczowi o adresie ania@wsei.edu.pl
+        var playerToChange = playersDictionary["ania@wsei.edu.pl"];
+        playerToChange = new Player()
+        {
+            Id = playerToChange.Id,
+            Name = playerToChange.Name,
+            Points = playerToChange.Points + 10
+        };
+        playersDictionary["ania@wsei.edu.pl"] = playerToChange;
+    }
+
+    class Player : IComparer<Player>, IComparable<Player>
+    {
+        public int Id { get; init; }
+        public string Name { get; init; }
+        public int Points { get; init; }
+
+        public override string ToString()
+        {
+            return $"{nameof(Id)}: {Id}, {nameof(Name)}: {Name}, {nameof(Points)}: {Points}";
+        }
+
+        protected bool Equals(Player other)
+        {
+            return Id == other.Id;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Player)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id;
+        }
+
+        public int Compare(Player x, Player y)
+        {
+            if (ReferenceEquals(x, y)) return 0;
+            if (ReferenceEquals(null, y)) return 1;
+            if (ReferenceEquals(null, x)) return -1;
+            return string.Compare(x.Name, y.Name, StringComparison.Ordinal);
+        }
+
+        public int CompareTo(Player? other)
+        {
+            if (ReferenceEquals(this, other)) return 0;
+            if (ReferenceEquals(null, other)) return 1;
+            return string.Compare(Name, other.Name, StringComparison.Ordinal);
+        }
+    } 
+}
+
