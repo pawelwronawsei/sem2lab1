02c6f73f10adaafb2338b11ca94534c18e72a495
pawel.wrona <pawel.wrona@microsoft.wsei.edu.pl>
2024-05-07 11:28:18 +0200

    commit

---
+++ lablib/EnumerableExtension.cs
@@ -0,0 +1,9 @@
+﻿namespace lablib;
+
+public static class EnumerableExtension
+{
+    public static void Print<T>(this IEnumerable<T> items)
+    {
+        Console.WriteLine(string.Join("\n", items));
+    }
+}
